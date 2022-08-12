c0418a27c43553b091e53984ee3d6566a2069021
mayankagrawal0004 <mayankagrawal0004@example.com>
2022-08-12 12:03:24 -0700

    file changes

--- 03-jsx/src/Details.js
+++ 03-jsx/src/Details.js
@@ -4,23 +4,26 @@ import ErrorBoundary from "./ErrorBoundary";
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true,showModal: false };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     );
     const json = await res.json();
-    this.setState(Object.assign({ loading: false }, json.pets[0]));
+    this.setState(Object.assign({ loading: false}, json.pets[0]));
   }
+
+  toggleModal = () => this.setState({showModal: !this.state.showModal});
 
   render() {
     if (this.state.loading) {
       return <h2>loading … </h2>;
     }
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -29,8 +32,20 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-          <button style={{backgroundColor: this.props.theme}}>Adopt {name}</button>
+          <button onClick={this.toggleModal} style={{backgroundColor: this.props.theme}}>Adopt {name}</button>
           <p>{description}</p>
+          
+      {showModal? (
+        <Modal>
+            <div>
+                <h1> Do you want to adopt {name}?</h1>
+                <div className="buttons">
+                <a href="https://bit.ly/pet-adopt">Yes</a>
+                <button onClick={this.toggleModal}>No</button>
+                </div>
+            </div>
+        </Modal>):null
+        }
         </div>
       </div>
     );
